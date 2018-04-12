134cf7e754692d4b3e4b1ace8b37037ce87183b3
Joy Ghosh <joyghosh@localhost.local>
2018-04-12 14:22:49 +0530

    second commit

--- fileupload/src/main/java/Example.java
+++ fileupload/src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example{
 	
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello Universe!";
 	}
 
 	public static void main(String[] args) throws Exception {
